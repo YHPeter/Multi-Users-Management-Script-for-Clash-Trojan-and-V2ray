27f5c17c03ed40e962721c2fcb7e2259152f8bf8
YHPeter <yuhao2003@hotmail.com>
2020-12-20 13:04:07 +0800

    Update: README.md; demo.yaml Add: support trojan-go, operating notations Rewrite: script written as Object Oriented

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Multi Users Management Script for Clash Trojan and V2ray
+# Multi Users Management Script for Clash, Trojan, Trojan-go and V2ray
 
 ## 安装（系统为 Centos7，其余还未测试）
 
@@ -12,21 +12,24 @@ git clone https://github.com/YHPeter/Multi-VPSs-Users-Management-Script-for-Troj
 
 目录结构：
 
-```
+``` tree
 ├─clash+v2ray+trojan多用户管理脚本.py
 ├─clash
 ├─demo.yaml
 ├─config.yaml
+├─v2ray
+│   └─config.json
 ├─trojan
-│  └─config.json
-└─v2ray
+│   └─config.json
+└─trojan-go
     └─config.json
 ```
+
 ## 使用脚本
 
 进入脚本所在目录，根据自己的需求修改demo.yaml文件和config.yaml文件，然后运行脚本：
 
-```python3 clash+v2ray+trojan多用户管理脚本.py ```
+```python3 clash+v2ray+trojan多用户管理脚本.py```
 
 ## 定时任务
 
@@ -36,11 +39,22 @@ git clone https://github.com/YHPeter/Multi-VPSs-Users-Management-Script-for-Troj
 
 ```crontab -e``` 对crontab进行编辑
 
-在其中增加如下的内容（每天凌晨一点运行脚本），注意python的版本用到了 python3
+在其中增加如下的内容（每天凌晨一点运行脚本），注意python的版本用到 python3
 
 ```00 1 * * * /usr/bin/python3 clash+v2ray+trojan多用户管理脚本.py```
 　　
-
 完成后，可以重启一下crontab的服务即可。
 
 ```service crond restart```
+
+## 功能
+
+- 根据demo.yaml自动所有生成用户独立的clash订阅文件
+  - v2ray uuid = ```str(uuid.uuid3(uuid.NAMESPACE_DNS, trojan/torjan-go password)```
+  - trojan password = password in config.yaml
+  - trojan-go password = password in config.yaml
+- 根据expire时间，自动切断v2ray和trojan连接（精准到天）
+  - 每天定时修改config文件
+  - 重启相应程序，使修改生效
+
+### TIPS: yaml.dump()写入scripts字段到文件时，会自动加'\\' 和 '\n'，但实际上是没有区别的（clash不会报错）！
--- clash+v2ray+trojan多用户管理脚本.py
+++ clash+v2ray+trojan多用户管理脚本.py
@@ -1,82 +1,114 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-# pip install pyyaml
-import os,time,json,yaml,uuid
+# pip install pyyaml,uuid
+import os, time, json, yaml, uuid, platform
 
-def updateTrojan(pwd_list):
-    with open(trojan_config, 'r') as f:
-        data = json.load(f)
+class Management():
+    def __init__(self) -> None:
 
-    data['password'] = pwd_list
+        with open('config.yaml',encoding='utf-8') as f:
+            config = yaml.safe_load(f)
 
-    with open(trojan_config,'w') as f:
-        json.dump(data,f,indent=4,ensure_ascii=False)
+        try:
+            pwdList = []
+            clash_change = config['是否更改clash配置']
+            v2ray_change = config['是否更改v2ray配置']
+            trojan_change = config['是否更改trojan配置']
+            trojan_go_change = config['是否更改trojan-go配置']
+            users_info = config['users']
+            for user in users_info:
+                password,expire = user['password'],user['expire']
+                expiration = time.mktime((expire.year, expire.month, expire.day, 0, 0, 0, 0, 0, 0))
+                if expiration>=time.time(): pwdList.append(password)
+        except Exception as e:
+            print('读取config.yaml存在%s错误，请检查！'%e)
+            exit()
 
-    # os.system('systemctl restart trojan')
-    # os.system('systemctl status trojan -l')
+        print('获取到以下未过期密码：')
+        for i in pwdList:
+            print(i)
+        
+        self.env = platform.system().lower()
 
-def UpdateV2ray(pwd_list):
-    with open(v2ray_config,'r') as f:
-        data = json.load(f)
+        if trojan_change: self.updateTrojan(config['trojan配置文件路径'], pwdList)
+        if trojan_go_change: self.updateTrojanGo(config['trojan-go配置文件路径'], pwdList)
+        if v2ray_change: self.UpdateV2ray(config['v2ray配置文件路径'], pwdList)
+        if clash_change: self.UpdateClash(config['demo.yaml文件夹路径'], config['clash文件输出目录'], pwdList)
 
-    data['inbounds'][0]['settings']['clients'] = [{'id':str(uuid.uuid3(uuid.NAMESPACE_DNS, pwd)), 'level':1, "alterId": 64} for pwd in pwd_list]
+    def UpdateV2ray(self, v2ray_config, pwd_list) -> None:
+        with open(v2ray_config,'r') as f:
+            data = json.load(f)
 
-    with open(v2ray_config, 'w') as f:
-        json.dump(data,f,indent=4,ensure_ascii=False)
+        data['inbounds'][0]['settings']['clients'] = [{'id':str(uuid.uuid3(uuid.NAMESPACE_DNS, pwd)), 'level':1, "alterId": 64} for pwd in pwd_list]
 
-    # os.system('systemctl restart v2ray')
-    # os.system('systemctl status v2ray -l')
-    
-def UpdateClash(trojan_pwdList):
-    with open(demo_yaml, 'r', encoding = 'utf-8') as f:
-        demo = yaml.load(f)
+        with open(v2ray_config, 'w') as f:
+            json.dump(data,f,indent=4,ensure_ascii=False)
 
-    os.system('rm -f %s*.yaml'%clash_output)
+        if self.env == 'linux':
+            try:
+                os.system('systemctl restart v2ray') # 
+                os.system('systemctl status v2ray -l')
+            except Exception as e:
+                print('配置修改成功，但发生%s错误，请检查！'%e)
+        else: print('不是linux系统，无法执行重启V2ray指令')
 
-    for trojanpwd in trojan_pwdList:
-        with open(os.path.join(clash_output,trojanpwd+'.yaml'), 'w') as f:
-            data = demo.copy()
-            proxies = []
-            proxy = demo['proxies']
-            for node in proxy:
-                if node['type']=='trojan':
-                    node['password'] = trojanpwd
+    def updateTrojan(self, trojan_config, pwd_list) -> None:
+        with open(trojan_config, 'r') as f:
+            data = json.load(f)
 
-                if node['type']=='vmess':
-                    node['password'] = str(uuid.uuid3(uuid.NAMESPACE_DNS, trojanpwd))
+        data['password'] = pwd_list
 
-                proxies.append(node)
+        with open(trojan_config,'w') as f:
+            json.dump(data,f,indent=4,ensure_ascii=False)
 
-            data['proxies'] = proxies
+        if self.env == 'linux':
+            try:
+                os.system('systemctl restart trojan')
+                os.system('systemctl status trojan -l')
+            except Exception as e:
+                print('配置修改成功，但发生%s错误，请检查！'%e)
+        else: print('不是linux系统，无法执行重启Trojan指令')
 
-            yaml.dump(data,f,allow_unicode=True,sort_keys=False,indent=4)
+    def updateTrojanGo(self, trojan_go_config, pwd_list) -> None:
+        with open(trojan_go_config, 'r') as f:
+            data = json.load(f)
 
-def main():
-    trojan_pwdList = []
-    for user in users_info:
-        password,expire = user['password'],user['expire']
-        expiration = time.mktime((expire.year, expire.month, expire.day, 0, 0, 0, 0, 0, 0))
-        if expiration>=time.time(): trojan_pwdList.append(password)
+        data['password'] = pwd_list
 
-    for i in trojan_pwdList:
-        print(i)
-    
-    if trojan_change: updateTrojan(trojan_pwdList)
-    if v2ray_change: UpdateV2ray(trojan_pwdList)
-    if clash_change: UpdateClash(trojan_pwdList)
+        with open(trojan_go_config,'w') as f:
+            json.dump(data,f,indent=4,ensure_ascii=False)
 
-if  __name__ == "__main__":
+        if self.env == 'linux':
+            try:
+                os.system('systemctl restart trojan-go')
+                os.system('systemctl status trojan-go -l')
+            except Exception as e:
+                print('配置修改成功，但发生%s错误，请检查！'%e)
+        else: print('不是linux系统，无法执行重启Trojan-go指令')
 
-    with open('config.yaml',encoding='utf-8') as f:
-        config = yaml.load(f)
+    def UpdateClash(self, demo_yaml,clash_output,trojan_pwdList) -> None:
+        with open(demo_yaml, 'r', encoding = 'utf-8') as f:
+            demo = yaml.safe_load(f)
 
-    demo_yaml = config['demo.yaml文件夹路径']
-    clash_output = config['clash文件输出目录']
-    trojan_config = config['trojan配置文件路径']
-    v2ray_config = config['v2ray配置文件路径']
-    trojan_change = config['是否更改trojan配置']
-    v2ray_change = config['是否更改v2ray配置']
-    clash_change = config['是否更改clash配置']
-    users_info = config['users']
+        os.system('rm -f %s*.yaml'%clash_output)
 
-    main()
+        for trojanpwd in trojan_pwdList:
+            with open(os.path.join(clash_output,trojanpwd+'.yaml'), 'w') as f:
+                data = demo.copy()
+                proxies = []
+                proxy = demo['proxies']
+                for node in proxy:
+                    if node['type']=='trojan':
+                        node['password'] = trojanpwd
+
+                    if node['type']=='vmess':
+                        node['password'] = str(uuid.uuid3(uuid.NAMESPACE_DNS, trojanpwd))
+
+                    proxies.append(node)
+
+                data['proxies'] = proxies
+
+                yaml.dump(data,f,allow_unicode=True,sort_keys=False,indent=4)
+
+if __name__ == "__main__":
+    Management()
